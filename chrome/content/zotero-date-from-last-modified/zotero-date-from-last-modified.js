b07c70f84e440c4d74d31879a99600151dbf92eb
friflaj <friflaj@ajellito.com>
2013-10-26 15:26:29 +0200

    initial release

---
+++ chrome/content/zotero-date-from-last-modified/zotero-date-from-last-modified.js
@@ -0,0 +1,45 @@
+Zotero.DateFromLastModified = {
+  init: function () {
+    // Register the callback in Zotero as an item observer
+    var notifierID = Zotero.Notifier.registerObserver(this.notifierCallback, ['item']);
+
+    console.log('grabbing web dates');
+    
+    // Unregister callback when the window closes (important to avoid a memory leak)
+    window.addEventListener('unload', function(e) {
+        Zotero.Notifier.unregisterObserver(notifierID);
+    }, false);
+  },
+  
+  // Callback implementing the notify() method to pass to the Notifier
+  notifierCallback: {
+    notify: function(event, type, ids, extraData) {
+      console.log(event);
+      if (event == 'add' || event == 'modify') {
+        var items = Zotero.Items.get(ids);
+        var item, url, date;
+
+        for (item of items) {
+          url = item.getField('url');
+          date = item.getField('date');
+          console.log('url=' + url, ', date=' + date);
+          if (url && !date) {
+            var req = new XMLHttpRequest();
+            req.open('GET', url, false);
+            req.send(null);
+            if (req.status == 200) {
+              date = req.getResponseHeader("Last-Modified");
+              if (date && date != '') {
+                item.setField('date', date);
+                item.save();
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+// Initialize the utility
+window.addEventListener('load', function(e) { Zotero.DateFromLastModified.init(); }, false);
